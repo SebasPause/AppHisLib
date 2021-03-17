17e7cf32e5f57dae3284db85b15aa8c1643972aa
Sebastian <adriansebastian.pauseniuc@iesvallecidacos.com>
2021-03-17 01:38:47 +0100

    VERSION FINAL

--- app/src/main/java/com/example/AppHisLib/presentacion/MainActivity.java
+++ app/src/main/java/com/example/AppHisLib/presentacion/MainActivity.java
@@ -1,37 +1,28 @@
 package com.example.AppHisLib.presentacion;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.AppHisLib.R;
 import com.example.AppHisLib.casosdeuso.CrearEstructura;
-import com.example.AppHisLib.casosdeuso.Libros;
 import com.example.AppHisLib.datos.LibroBD;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements Serializable{
 
@@ -46,20 +37,6 @@ public class MainActivity extends AppCompatActivity implements Serializable{
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Bundle extras = getIntent().getExtras();
-        if(extras == null){
-            //no hace nada
-        }else{
-            String codigo = extras.getString("Codigo");
-            if(codigo.equals("cerrarSesion")){
-                usuario = null;
-                onRestart();
-            }
-        }
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
         /**
          * Inicializo una nueva base de datos
          * onUpgrade() para borrar todos los datos y volver a crear las tablas
@@ -67,8 +44,26 @@ public class MainActivity extends AppCompatActivity implements Serializable{
          * en le base de datos interna LibroBD
          */
         LibroBD bd = new LibroBD(this);
-        bd.onUpgrade(bd.getWritableDatabase(),1,2);
+        bd.onUpgrade(bd.getWritableDatabase(),1,1);
         bd.obtenerDatos();
+
+        Bundle extras = getIntent().getExtras();
+        if(extras == null){
+            //no hace nada
+        }else{
+            if(extras.getString("Codigo")==null){
+                //nada
+            }else{
+                String codigo = extras.getString("Codigo");
+                if(codigo.equals("cerrarSesion")){
+                    usuario = null;
+                    onRestart();
+                }
+            }
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
 
         /**
          * Datos relacionados al menu superior
